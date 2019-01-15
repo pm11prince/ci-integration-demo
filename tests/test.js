dedae9a3769d11c9b7296416ad5595fc82af3e61
pm11prince <pm11prince@gmail.com>
2019-01-15 12:41:00 +0530

    Update test.js
    updated test case

--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,7 @@ test('Text typing basics', async t => {
         .typeText(page.nameInput, 'Peter')                      // Type name
         .typeText(page.nameInput, 'Paker', { replace: true })   // Replace with last name
         .typeText(page.nameInput, 'r', { caretPos: 2 })         // Correct last name
-        .expect(page.nameInput.value).eql('Parker');            // Check result
+        .expect(page.nameInput.value).eql('Parker1');            // Check result
 });
 
 
